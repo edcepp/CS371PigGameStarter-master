8446639aad790f57b3d719c794bdaf05576a4ac3
edcepp <epp@zdome.net>
2017-03-04 09:26:21 -0800

    Part 2 Create PPigHoldAction and PigRollAction classes

---
+++ app/src/main/java/edu/up/cs301/pig/PigHoldAction.java
@@ -0,0 +1,13 @@
+package edu.up.cs301.pig;
+
+import edu.up.cs301.game.actionMsg.GameAction;
+import edu.up.cs301.game.GamePlayer;
+/**
+ * Created by eepp on 3/4/17.
+ */
+
+public class PigHoldAction extends GameAction {
+    public PigHoldAction (GamePlayer aPlayer){
+        super(aPlayer);
+    }
+}
---
+++ app/src/main/java/edu/up/cs301/pig/PigRollAction.java
@@ -0,0 +1,14 @@
+package edu.up.cs301.pig;
+
+import edu.up.cs301.game.actionMsg.GameAction;
+import edu.up.cs301.game.GamePlayer;
+
+/**
+ * Created by eepp on 3/4/17.
+ */
+
+public class PigRollAction extends GameAction {
+    public PigRollAction(GamePlayer aPlayer){
+        super (aPlayer);
+    }
+}
